dd543bee761bf47d742f581c5fa003895e3f0688
dangre0730 <130814842+dangre0730@users.noreply.github.com>
2026-06-26 11:12:00 -0700

    Created duplicate battleFrame method and renamed original to CONSOLEbattleFrame to begin the refactor to BF within the UI instead.

--- src/Functions.java
+++ src/Functions.java
@@ -306,7 +306,112 @@ public class Functions {
             slowPrint("You won the fight! Your closing health is " + playerHealth);
             Game.player.playerHealth = playerHealth;
 //            return earnedXP;
-            return Game.enemy.earnedExp;
+            return Game.enemyNPC.earnedExp;
+        } else {
+            return 0;
+        }
+        // while(())
+//        if(earnedXP < 3){
+//            return 3;
+//        } else {
+//            return earnedXP;
+//        }
+    }
+
+    public static int CONSOLEbattleFrame(int enemyHealth, int playerAttackDamage, int playerDamageMod, int enemyDamage, int enemyDamageMod, double attackSpeed, int playerArmor, int enemyArmor, double playerCritChance, double enemyCritChance){
+        //Had idea about storing all player and enemy stats in arrays and trying to pass the entire array to this method for input.
+        //That would make this much cleaner and easier to call in Main method.
+
+        int playerHealth = Game.player.playerHealth;
+        int earnedXP = 0;
+        int damageDealt = 0;
+        double extraAttack = 0.0;
+
+        Scanner getInput = new Scanner(System.in);
+        while((enemyHealth > 0) && (playerHealth > 0)){
+
+            System.out.println("What would you like to do?");
+            slowPrint("1. Attack  2. Move");
+            slowPrint("3. Mock    4. Run");
+            int frameSelection = getInput.nextInt();
+
+            if((frameSelection == 1) || (frameSelection == 2) || (frameSelection == 3) || (frameSelection == 4)) {
+                if (frameSelection == 1) {
+                    damageDealt = makeAnAttack(playerAttackDamage, playerDamageMod, enemyArmor, playerCritChance);
+                    extraAttack += attackSpeed - 1d;
+                    if(damageDealt > 0) {
+                        slowPrint("B A M !");
+                        enemyHealth -= damageDealt;
+//                        earnedXP += 3;
+                        slowPrint("Wow! You dealt " + damageDealt + " damage to the target.");
+                    } else {
+                        slowPrint("Player failed to deal damage this round");
+//                        earnedXP += 3;
+                    }
+                    if(enemyHealth > 0){
+                        slowPrint("Enemy Health is " + enemyHealth);
+                    } else {
+                        slowPrint("Enemy slain! You survived the battle.");
+                        break;
+                    }
+                    if(extraAttack >= 1){
+                        slowPrint("---EXTRA ATTACK---");
+                        damageDealt = makeAnAttack(playerAttackDamage, playerDamageMod, enemyArmor, playerCritChance);
+                        if(damageDealt > 0) {
+                            slowPrint("P O W !");
+                            enemyHealth -= damageDealt;
+//                            earnedXP += 3;
+                            slowPrint("Wow! You dealt " + damageDealt + " damage to the target.");
+                        } else {
+                            slowPrint("Player failed to deal damage this round");
+//                            earnedXP += 0;
+                        }
+                        if(enemyHealth > 0){
+                            slowPrint("Enemy Health is " + enemyHealth);
+                        } else {
+                            slowPrint("Enemy slain! You survived the battle.");
+                            break;
+                        }
+                        extraAttack = 0.0;
+                    }
+                } else if(frameSelection == 2) {
+                    slowPrint("The Movement Module doesn't exist yet.");
+                } else if(frameSelection == 3){
+                    slowPrint("The Mock Module doesn't exist yet.");
+                } else {
+                    slowPrint("Adventurer abandoned their Quest...");
+                    return 40;
+                }
+
+            } else {
+                slowPrint("That's not an option.");
+            }
+            if(enemyHealth > 0) {
+                slowPrint("Enemy Turn Begins.");
+                damageDealt = makeAnAttack(enemyDamage, enemyDamageMod, playerArmor, enemyCritChance);
+                if (damageDealt > 0) {
+                    playerHealth -= damageDealt;
+                } else {
+                    slowPrint("Enemy failed to deal damage this round");
+                }
+                if(playerHealth > 0){
+                    slowPrint("You took " + damageDealt + " damage.");
+                    slowPrint("Player health is " + playerHealth);
+                }
+            } else {
+                slowPrint("Enemy health has been reduced to 0.");
+            }
+        }
+//        if(playerHealth <= 0){
+//            System.out.println("You lost the fight");
+//            Game.player.playerHealth = playerHealth;
+//            return earnedXP;
+//        } else
+        if(enemyHealth <= 0){
+            slowPrint("You won the fight! Your closing health is " + playerHealth);
+            Game.player.playerHealth = playerHealth;
+//            return earnedXP;
+            return Game.enemyNPC.earnedExp;
         } else {
             return 0;
         }
